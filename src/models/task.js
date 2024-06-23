12b702e0e02f2cf922b2a42a6e47495a94c18d58
Samuel Javorka <samkoj@hotmail.co.uk>
2024-06-23 23:19:35 +0100

    enable timestamps for task schema

--- src/models/task.js
+++ src/models/task.js
@@ -4,22 +4,27 @@ https://www.npmjs.com/package/validator
 */
 const validator = require("validator");
 
-const taskSchema = mongoose.Schema({
-	description: {
-		type: String,
-		required: true,
-		trim: true,
+const taskSchema = new mongoose.Schema(
+	{
+		description: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		completed: {
+			type: Boolean,
+			default: false,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "User",
+		},
 	},
-	completed: {
-		type: Boolean,
-		default: false,
-	},
-	owner: {
-		type: mongoose.Schema.Types.ObjectId,
-		required: true,
-		ref: "User",
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const Task = mongoose.model("Task", taskSchema);
 
