286df86b850cd0b98428a741e81d4a1313f86a42
Samuel Javorka <samkoj@hotmail.co.uk>
2024-06-28 20:03:23 +0100

    create error handling for mongodb connecting

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		console.log("MongoDB connected.");
+	})
+	.catch((error) => {
+		console.error(error);
+	});
