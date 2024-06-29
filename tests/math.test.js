3fb9239fd0dcbb1c4adaf558bcd57b692ae674f3
Samuel Javorka <samkoj@hotmail.co.uk>
2024-06-29 23:49:44 +0100

    Testing: use jest's expect and toBe functions

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,5 +2,11 @@ const { calculateTip } = require("../src/math");
 
 test("Should calculate total with tip", () => {
 	const total = calculateTip(10, 0.3);
-	if (total !== 13) throw new Error(`Total should be 13. Got ${total}`);
+	// if (total !== 13) throw new Error(`Total should be 13. Got ${total}`);
+	expect(total).toBe(13);
+});
+
+test("Should calculate total with default tip", () => {
+	const total = calculateTip(10);
+	expect(total).toBe(12.5);
 });
