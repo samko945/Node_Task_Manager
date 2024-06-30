8acced8a7c2614fd9b2c37a5ef6e9d69df553678
Samuel Javorka <samkoj@hotmail.co.uk>
2024-06-30 11:46:15 +0100

    install env-cmd, create test.env with separate vars

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 // connect to db via mongoose
 require("./db/mongoose");
 const userRouter = require("./routers/user");
