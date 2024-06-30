f7b97fdb896fe40eb7232afa8a5edba41a86e2af
Samuel Javorka <samkoj@hotmail.co.uk>
2024-06-30 21:29:27 +0100

    Testing: use life cycle function beforeEach to wipe users and create a user for testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,18 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+	name: "Bob",
+	email: "bob@gmail.com",
+	password: "bob12345",
+};
+
+beforeEach(async () => {
+	// clear the users in database
+	await User.deleteMany();
+	await new User(userOne).save();
+});
 
 test("Should signup a new user", async () => {
 	await request(app)
@@ -10,4 +23,24 @@ test("Should signup a new user", async () => {
 			password: "sam12345",
 		})
 		.expect(201);
+});
+
+test("Should login existing user", async () => {
+	await request(app)
+		.post("/users/login")
+		.send({
+			email: userOne.email,
+			password: userOne.password,
+		})
+		.expect(200);
+});
+
+test("Should not login nonexistent user", async () => {
+	await request(app)
+		.post("/users/login")
+		.send({
+			email: "wrongEmail@yoyo.com",
+			password: "21dsa32",
+		})
+		.expect(400);
 });
