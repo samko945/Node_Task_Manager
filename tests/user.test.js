4f7963647f7927dc2b4af50cbd2628170c705aa7
Samuel Javorka <samkoj@hotmail.co.uk>
2024-06-30 21:09:25 +0100

    Test for user signup.  And update jest and other packages

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,13 @@
 const request = require("supertest");
+const app = require("../src/app");
+
+test("Should signup a new user", async () => {
+	await request(app)
+		.post("/users")
+		.send({
+			name: "Sam",
+			email: "samjavorka@gmail.com",
+			password: "sam12345",
+		})
+		.expect(201);
+});
