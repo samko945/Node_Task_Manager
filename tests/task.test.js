458dea8003ab0dff898c89443685fe68f80c1517
Samuel Javorka <samkoj@hotmail.co.uk>
2024-07-03 22:13:24 +0100

    Testing: task with filter param

--- tests/task.test.js
+++ tests/task.test.js
@@ -37,3 +37,22 @@ test("Should not delete other users tasks", async () => {
 	const task = await Task.findById(taskOne._id);
 	expect(task).not.toBeNull();
 });
+
+test("Should fetch only incomplete tasks", async () => {
+	const response = await request(app)
+		.get("/tasks?completed=false")
+		.set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+		.send()
+		.expect(200);
+	expect(response.body.every((task) => task.complete === false)).toBe(false);
+});
+
+/*
+ Task Test Ideas
+ - Should not create task with invalid description / completed
+ - Should not update task with invalid description / completed
+ - Should delete user task
+ - Should not delete task if unauthenticated
+ - Should sort tasks by description / completed / createdAt / updatedAt
+ - Should fetch page of tasks
+*/
